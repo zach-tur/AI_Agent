87147440948a57e4b6971f89224ebb5962038c18
zach-tur <108747741+zach-tur@users.noreply.github.com>
2025-08-21 19:10:43 -0700

    added run_python_file

---
+++ functions/run_python.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+from sys import stderr, stdout
+from threading import ExceptHookArgs
+
+
+def run_python_file(working_directory, file_path, args=[]):
+    full_path = os.path.join(working_directory, file_path)
+    abs_path = os.path.abspath(full_path)
+    abs_working = os.path.abspath(working_directory)
+    if not abs_path.startswith(abs_working):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.exists(abs_path):
+        return f'Error: File "{file_path}" not found.'
+
+    if not abs_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+
+    try:
+        completed_process = subprocess.run(
+            ["python", file_path, *args],
+            capture_output=True,
+            cwd=abs_working,
+            timeout=30,
+        )
+
+        if completed_process.returncode != 0:
+            return f"Process exited with code {completed_process.returncode}"
+        if completed_process.stdout == None:
+            return "No output produced."
+
+        if not completed_process.stdout and not completed_process.stderr:
+            return f"No output produced."
+
+        return f"STDOUT:\n{completed_process.stdout}\nSTDERR:\n{completed_process.stderr}\n"
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -1,44 +1,25 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 
-# def test_get_files_info():
-#    result = get_files_info("calculator", ".")
-#    print(f"Result for current directory:")
-#    print(result)
-#
-#    result = get_files_info("calculator", "pkg")
-#    print(f"Result for 'pkg' directory:")
-#    print(result)
-#
-#    result = get_files_info("calculator", "/bin")
-#    print(f"Result for '/bin' directory:")
-#    print(result)
-#
-#    result = get_files_info("calculator", "../")
-#    print(f"Result for '../' directory:")
-#    print(result)
+def test():
+    result = run_python_file("calculator", "main.py")
+    print(f"First result:\n{result}")
 
+    result = run_python_file("calculator", "main.py", ["3 + 5"])
+    print(f"Second result:\n{result}")
 
-# def test_get_file_content():
-#    result = get_file_content("calculator", "lorem.txt")
-#    print(result)
+    result = run_python_file("calculator", "tests.py")
+    print(f"Third result:\n{result}")
 
+    result = run_python_file("calculator", "../main.py")
+    print(f"Fourth result:\n{result}")
 
-def test_get_file_content():
-    result = get_file_content("calculator", "main.py")
-    print(result)
-
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print(result)
-
-    result = get_file_content("calculator", "bin/cat")
-    print(result)
-
-    result = get_file_content("calculator", "pkg/does_not_exist.py")
-    print(result)
+    result = run_python_file("calculator", "nonexistent.py")
+    print(f"Fifth result:\n{result}")
 
 
 if __name__ == "__main__":
-    #    test_get_files_info()
-    test_get_file_content()
+    test()
